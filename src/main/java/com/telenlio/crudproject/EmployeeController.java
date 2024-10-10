ec05d541d4fdb8d013dbc55fa30b15f611f5ee25
harshpaliwal0420 <harshpaliwal0420@gmail.com>
2024-10-11 03:51:56 +0530

    second commit

--- src/main/java/com/telenlio/crudproject/EmployeeController.java
+++ src/main/java/com/telenlio/crudproject/EmployeeController.java
@@ -5,10 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-// import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
-// @CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1")
 public class EmployeeController {
